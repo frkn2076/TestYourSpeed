8cd22dc684026e778dcf630cd20e78db744f2e7b
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-03-07 00:10:10 +0300

    manager controller edited

--- controller/manager.go
+++ controller/manager.go
@@ -9,13 +9,17 @@ type ManagerController struct{}
 
 func (u *ManagerController) Test(c *gin.Context) {
 	testerId := c.Param("testerId")
-	var res float64
+	var dwnld float64
+	var upld float64
 	if testerId == "1" {
-		res, _ = service.FastDownloadTest()
+		dwnld, upld, _ = service.TestFast()
 	} else if testerId == "2" {
-		res, _ = service.SpeedTestDownloadTest()
+		dwnld, upld, _ = service.TestSpeedTest()
 	} else {
 		c.AbortWithStatus(400)
 	}
-	c.JSON(200, res)
+	c.JSON(200, gin.H{
+		"Download": dwnld,
+		"Upload": upld,
+	})
 }
