dd15075cd93678b363d8e0845a6944a18f88c8cf
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-03-07 00:08:33 +0300

    speedtest upload speed added

--- service/speedtest.go
+++ service/speedtest.go
@@ -15,34 +15,24 @@ func init() {
 	}
 }
 
-func SpeedTestDownloadTest() (float64, error) {
+func TestSpeedTest() (float64, float64, error) {
 
 	server, err := singleSpeedTest.GetServer("")
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
 	dmbps, err := singleSpeedTest.Download(server)
 	if err != nil {
-		return 0, err
-	}
-
-	result := math.Floor(dmbps*100) / 100
-	return result, nil
-}
-
-func SpeedTestUploadTest() (float64, error) {
-
-	server, err := singleSpeedTest.GetServer("")
-	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
 	umbps, err := singleSpeedTest.Upload(server)
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
-	result := math.Floor(umbps*100) / 100
-	return result, nil
+	dwnld := math.Floor(dmbps*100) / 100
+	upld := math.Floor(umbps*100) / 100
+	return dwnld, upld, nil
 }
